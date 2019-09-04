50912c7dc20c5626e5b2eb407ca2a316f7af8387
saji yogie permana <sajiyogipermana@gmail.com>
2019-09-04 19:26:43 +0700

    commit pertama saji

---
+++ database/migrations/2019_09_04_092327_rules.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Rules extends Migration
+{
+    public function up()
+    {
+        Schema::create('Rules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('judul');
+            $table->longText('description')->nullable();
+            $table->string('pengesah');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('Rules');
+    }
+}
